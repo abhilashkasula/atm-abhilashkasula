2410ebe56be8a3f554a51309a19a4cf913784022
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-04-07 15:12:03 +0530

    added left shift operation and removed #defines

--- atm.c
+++ atm.c
@@ -1,31 +1,15 @@
 #include "atm.h"
 
-#define ones 0x1
-#define fives 0x10
-#define tens 0x100
-#define twenties 0x1000
-#define fifties 0x10000
-#define hundreds 0x100000
-#define five_hundreds 0x1000000
-#define two_thousands 0x10000000
-
 unsigned int get_money(unsigned short int amount) {
-  unsigned int notes = 0x0;
-  if(amount > 31999) return 0;
-  notes += two_thousands * (amount / 2000);
-  amount = amount % 2000;
-  notes += five_hundreds * (amount / 500);
-  amount = amount % 500;
-  notes += hundreds * (amount / 100);
-  amount = amount % 100;
-  notes += fifties * (amount / 50);
-  amount = amount % 50;
-  notes += twenties * (amount / 20);
-  amount = amount % 20;
-  notes += tens * (amount / 10);
-  amount = amount % 10;
-  notes += fives * (amount / 5);
-  amount  = amount % 5;
-  notes += ones * (amount / ones);
-  return notes;
+  unsigned int counts = 0x0;
+  unsigned short int remaining = amount;
+  unsigned short int notes[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
+  if(remaining <= 31999) {
+    for(int i = 0; i < 8; i++) {
+      counts = counts << 4;
+      counts += remaining / notes[i];
+      remaining %= notes[i];
+    }
+  }
+  return counts;
 }
