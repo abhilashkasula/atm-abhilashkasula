f2d8001041bed51ffa01a80b4ed4009cff015a97
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-04-07 16:46:51 +0530

    added function display_notes

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "atm.h"
 
+void display_notes(unsigned int counts) {
+  unsigned int notes[] = {1, 5, 10, 20, 50, 100, 500, 2000};
+  for(int i = 0; i < 8; i++) {
+    printf("%u notes of %u\n", counts & 0xf, notes[i]);
+    counts = counts >> 4;
+  }
+}
+
 int main(void)
 {
   unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
@@ -8,6 +16,7 @@ int main(void)
   {
     unsigned int notes = get_money(amounts[i]);
     printf("%5u %08x\n", amounts[i], notes);
+    display_notes(notes);
   }
 
   return 0;
